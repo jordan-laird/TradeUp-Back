273043f967aac8e2d6fc16fe7d7c199947c078e0
github username <github email address>
2018-12-18 16:58:25 -0600

    stocks controller done

--- server/app/controllers/api/v1/stocks_controller.rb
+++ server/app/controllers/api/v1/stocks_controller.rb
@@ -1,2 +1,43 @@
 class Api::V1::StocksController < Api::V1::ApplicationController
+    before_action :define_current_stock
+
+    def create
+        stock = Stock.create(stock_params)
+        render json: stock 
+    end
+
+    def index
+        render json: Stock.all
+    end
+
+    def show
+        render json: current_stock
+    end
+
+    def update
+        current_stock.update(stock_params)
+        render json: current_stock
+    end
+
+    def destroy
+        current_stock.destroy
+        render json: current_stock
+    end
+
+    def stock_params
+        params.require(:stock).permit(:account_symbol)
+    end
+
+    def define_current_stock
+        if params[:id]
+            @current_stock = Stock.find(params[:id])
+        else
+            @current_stock = Stock.new
+        end
+    end
+
+    def current_stock
+        @current_stock
+    end
+    
 end
--- server/app/controllers/api/v1/users_controller.rb
+++ server/app/controllers/api/v1/users_controller.rb
@@ -1,45 +1,45 @@
 class Api::V1::UsersController < Api::V1::ApplicationController
     
-    # before_action :define_current_user
+    before_action :define_current_user
 
-    # skip_before_action :check_authentication, only: [ :index ]
+    skip_before_action :check_authentication, only: [ :index ]
     
-    # def create
-    #     user = User.create(user_params)
-    #     render json: user
-    # end
+    def create
+        user = User.create(user_params)
+        render json: user
+    end
     
-    # def index
-    #     render json: User.all
-    # end
+    def index
+        render json: User.all
+    end
     
-    # def show
-    #     render json: current_user
-    # end
+    def show
+        render json: current_user
+    end
     
-    # def update
-    #     current_user.update(user_params)
-    #     render json: current_user
-    # end
+    def update
+        current_user.update(user_params)
+        render json: current_user
+    end
     
-    # def destroy
-    #     current_user.destroy
-    #     render json: current_user
-    # end
+    def destroy
+        current_user.destroy
+        render json: current_user
+    end
     
-    # def user_params
-    #     params.permit(:email, :password, :image_url, :name, :phone)
-    # end
+    def user_params
+        params.permit(:first_name, :last_name, :email, :password)
+    end
     
-    # def define_current_user
-    #     if params[:id]
-    #         @current_user = User.find(params[:id])
-    #     else
-    #         @current_user = User.new
-    #     end
-    # end
+    def define_current_user
+        if params[:id]
+            @current_user = User.find(params[:id])
+        else
+            @current_user = User.new
+        end
+    end
     
-    # def current_user
-    #     @current_user
-    # end
+    def current_user
+        @current_user
+    end
 end
